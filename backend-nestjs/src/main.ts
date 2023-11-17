83f6e22ee8479baae7b6557038c9e10e49a6ccff
andysign <andysignerdesigner@gmail.com>
2023-11-17 22:40:09 +0000

    Added modfied main ts . It now includes swagger and cors and other stuff

--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import 'dotenv/config'; require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('example')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
