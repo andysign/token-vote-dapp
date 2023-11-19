f61a995f6849aa97c735b4283477ed8a51d5fa80
Bang-Tam Nguyen <bang-tam.nguyen@fujitsu.com>
2023-11-19 20:18:55 +0100

    add vote form, add fetch abi from useContractRead, fix request token body

--- frontend-scaffoldeth/packages/nextjs/components/tokenized-ballot/TokenizedBallotPage.tsx
+++ frontend-scaffoldeth/packages/nextjs/components/tokenized-ballot/TokenizedBallotPage.tsx
@@ -2,27 +2,43 @@ import { useEffect, useState } from "react";
 import { DeployCard } from "./DeployCard";
 import { TokenizedBallotInformation } from "./TokenizedBallotInformation";
 
+export interface TokenizedBallotInfo {
+  name: string;
+  address: `0x${string}`;
+}
 export const TokenizedBallotPage = ({}) => {
-  const [tokenizedBallot, setTokenizedBallot] = useState({ name: "mock", address: "" });
-  const [isLoading, setLoading] = useState(false);
+  const [tokenizedBallot, setTokenizedBallot] = useState<TokenizedBallotInfo>();
+  const [tokenizedBallotAbi, setTokenizedBallotAbi] = useState();
+  const [isLoading, setLoading] = useState(true);
 
   const fetchTokenizedBallotInfo = async () => {
-    // await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tokenized-ballot`)
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     setTokenizedBallot(data);
-    //     setLoading(false);
-    //   });
-    setTokenizedBallot({ name: "", address: "" });
-    setLoading(false);
+    await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/contract-ballot-address`)
+      .then(res => res.json())
+      .then(data => {
+        if (data && data.result) setTokenizedBallot({ name: "Tokenized Ballot", address: data.result });
+        setLoading(false);
+      })
+      .catch(error => console.log(error));
   };
 
-  const isDeployed = tokenizedBallot.address ?? false;
+  const fetchTokenizedBallotAbi = async () => {
+    await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/contract-ballot-abi`)
+      .then(res => res.json())
+      .then(data => {
+        if (data && data.result) setTokenizedBallotAbi(data.result);
+        setLoading(false);
+      })
+      .catch(error => console.log(error));
+  };
+
+  const isDeployed = tokenizedBallot?.address ?? false;
 
   useEffect(() => {
     fetchTokenizedBallotInfo();
+    fetchTokenizedBallotAbi();
   }, []);
-  if (isLoading) return <p>Fetching Tokenized Ballot contract...</p>;
+  if (isLoading || !tokenizedBallot || !tokenizedBallotAbi) return <p>Fetching Tokenized Ballot contract...</p>;
   if (!isDeployed) return <DeployCard />;
-  return <TokenizedBallotInformation tokenizedBallot={tokenizedBallot} />;
+
+  return <TokenizedBallotInformation tokenizedBallot={tokenizedBallot} tokenizedBallotAbi={tokenizedBallotAbi} />;
 };
