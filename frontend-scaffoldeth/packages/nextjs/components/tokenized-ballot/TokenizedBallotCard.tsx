ccdd8a9f0de67487a4c323522395ea02aefdd3e8
andysign <andysignerdesigner@gmail.com>
2023-11-20 02:31:10 +0000

    Added Tokenized General Info and Tokenized Card

---
+++ frontend-scaffoldeth/packages/nextjs/components/tokenized-ballot/TokenizedBallotCard.tsx
@@ -0,0 +1,84 @@
+import { TokenizedBallotInfo } from "./TokenizedBallotPage";
+import { TokenizedBallotVote } from "./TokenizedBallotVote";
+import { useAccount, useContractRead } from "wagmi";
+
+interface TokenizedBallotInformationComponent {
+  tokenizedBallot: TokenizedBallotInfo;
+  tokenizedBallotAbi: Array<any>;
+}
+
+export const TokenizedBallotCard = ({
+  tokenizedBallot,
+  tokenizedBallotAbi,
+}: TokenizedBallotInformationComponent) => {
+  const { address } = useAccount();
+
+  // fetch balance
+  const { data: balanceUser } = useContractRead({
+    address: process.env.NEXT_PUBLIC_TOKEN_ADDRESS,
+    abi: [
+      {
+        constant: true,
+        inputs: [
+          {
+            name: "_owner",
+            type: "address",
+          },
+        ],
+        name: "balanceOf",
+        outputs: [
+          {
+            name: "balance",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    functionName: "balanceOf",
+    args: [address],
+  });
+
+  // fetch hasVoted
+  const { data: hasVoted } = useContractRead({
+    address: tokenizedBallot.address, // could use NEXT_PUBLIC_BALLOT_ADDRESS in dev mode
+    abi: tokenizedBallotAbi,
+    functionName: "hasAlreadyVoted",
+    args: [address],
+  });
+  // fetch self delegate
+  const { data: votingPower } = useContractRead({
+    address: tokenizedBallot.address,
+    abi: tokenizedBallotAbi,
+    functionName: "votingPower",
+    args: [address],
+  });
+
+  const balance = typeof balanceUser === "number" || typeof balanceUser === "bigint" ? balanceUser : 0;
+  const hasBalance = balance !== 0;
+
+  const votingPowerDigit = typeof votingPower === "number" || typeof balanceUser === "bigint" ? balanceUser : 0;
+  const hasVotingPower = votingPowerDigit !== 0;
+
+  return (
+    <>
+      <div style={{ margin: "5rem" }}>
+        <h2 className="block text-4xl font-bold text-center">VOTE ACCESS</h2>
+
+        <div className="text-center m-4">
+          {!hasBalance && (
+            <div className="text-error"> You cannot access the vote yet, you dont have enough tokens</div>
+          )}
+          {hasVoted && <div className="text-error"> You cannot access the vote, you have already voted</div>}
+          {!hasVotingPower && (
+            <div className="text-error"> You cannot access the vote, you need to self delegate before</div>
+          )}
+
+          {hasBalance && !hasVoted && hasVotingPower && <TokenizedBallotVote />}
+        </div>
+      </div>
+    </>
+  );
+};
